239e970d213ecfa1cf822ddf113547fd2fc023d6
Chaitanya Gupta <chaitanyagupta2809@gmail.com>
2026-07-14 21:37:58 +0530

    Added login page

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ from visualization import generate_pie_chart, generate_bar_chart, generate_reaso
 app = Flask(__name__)
 
 @app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 def home():
 
     return render_template(
